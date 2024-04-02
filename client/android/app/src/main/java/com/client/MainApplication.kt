77711006d22aefd5d33c91117f0031608e2c64d1
Nicolas Bonifácio Pereira <nicolas.bonifa@outlook.com>
2024-04-02 17:51:06 -0300

    Otimizações nas pastas

--- client/android/app/src/main/java/com/client/MainApplication.kt
+++ client/android/app/src/main/java/com/client/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.codigosdoapi
+package com.client
 
 import android.app.Application
 import com.facebook.react.PackageList
--- client/ios/clientTests/clientTests.m
+++ client/ios/clientTests/clientTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface CodigosDoAPITests : XCTestCase
+@interface clientTests : XCTestCase
 
 @end
 
-@implementation CodigosDoAPITests
+@implementation clientTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
