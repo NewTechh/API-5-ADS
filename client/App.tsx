05522877c6aa85887404bc052697a117db45d0c8
Thalles Torres <thalles.torres@pm.me>
2024-04-08 18:06:16 -0300

    Melhoria App.tsx

--- client/App.tsx
+++ client/App.tsx
@@ -1,39 +1,14 @@
-import React, { useState } from 'react';
-import { View, StyleSheet, ScrollView } from 'react-native';
-import Footer from './src/components/Footer';
-import SideMenu from './src/components/SideMenu';
-import { SignUp } from './src/components/SignUp';
-import DashboardPartner from './src/components/DashboardPartner';
-import LoginScreen from './src/components/LoginScreen';
+import React from 'react';
+import { StatusBar, SafeAreaView } from 'react-native';
 import Rotas from './routes/Rotas';
 
 const App = () => {
-    const [isSideMenuVisible, setIsSideMenuVisible] = useState(false);
-
-    const toggleSideMenu = () => {
-        setIsSideMenuVisible(!isSideMenuVisible);
-    };
-
     return (
-        <View style={styles.container}>
+        <SafeAreaView style={{flex: 1}}>
+            <StatusBar backgroundColor={'#272424'} barStyle={'light-content'}/>
             <Rotas />
-            {/* <ScrollView contentContainerStyle={styles.scrollViewContent}>
-                <DashboardPartner />
-            </ScrollView>
-            <Footer onPressMenu={toggleSideMenu} />
-            {isSideMenuVisible && <SideMenu onClose={toggleSideMenu} />} */}
-        </View>
+        </SafeAreaView>
     );
 };
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    scrollViewContent: {
-        flexGrow: 1,
-        paddingBottom: 1,
-    },
-});
 
 export default App;
