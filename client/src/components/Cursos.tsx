0d2bae07584ade08a2aafff0fc970a6021bdac61
Thalles Torres <thalles.torres@pm.me>
2024-04-08 18:08:27 -0300

    Add botoes

--- client/src/components/Cursos.tsx
+++ client/src/components/Cursos.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity, StatusBar } from "react-native";
+import { View, Text, Pressable } from "react-native";
 import { styles } from '../styles/curse';
 import * as Progress from 'react-native-progress';
+import { AntDesign } from "@expo/vector-icons";
 
 export function Cursos() {
     const [progress, setProgress] = useState(0.5);
@@ -13,22 +14,31 @@ export function Cursos() {
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Cursos e Treinamentos</Text>
-            <TouchableOpacity onPress={handlePress} style={styles.button1}>
+            <Pressable style={styles.iconPlus}>
+                <AntDesign
+                    name={'pluscircleo'}
+                    size={35}
+                    color='white'
+                    
+                    // onPress={}
+                />
+            </Pressable>
+            <Pressable onPress={handlePress} style={styles.button}>
                 <Text style={styles.buttonText}>IaaS & PaaS</Text>
                 <Progress.Bar progress={progress} width={380} color={'#17E753'} />
-            </TouchableOpacity>
-            <TouchableOpacity onPress={handlePress} style={[styles.button2]}>
+            </Pressable>
+            <Pressable onPress={handlePress} style={[styles.button]}>
                 <Text style={styles.buttonText}>CX</Text>
                 <Progress.Bar progress={progress} width={380} color={'#17E753'} />
-            </TouchableOpacity>
-            <TouchableOpacity onPress={handlePress} style={[styles.button3]}>
+            </Pressable>
+            <Pressable onPress={handlePress} style={[styles.button]}>
                 <Text style={styles.buttonText}>Industries</Text>
                 <Progress.Bar progress={progress} width={380} color={'#17E753'} />
-            </TouchableOpacity>
-            <TouchableOpacity onPress={handlePress} style={[styles.button4]}>
+            </Pressable>
+            <Pressable onPress={handlePress} style={[styles.button]}>
                 <Text style={styles.buttonText}>License & Hardware</Text>
                 <Progress.Bar progress={progress} width={380} color={'#17E753'} />
-            </TouchableOpacity>
+            </Pressable>
         </View>
     )
 }
