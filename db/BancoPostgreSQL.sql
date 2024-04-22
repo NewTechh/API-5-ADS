d7f360fe83cebee22752bd0a5687ffc1d96562d2
Francisco Quirino <quirino1988@gmail.com>
2024-04-22 19:39:32 -0300

    atualizando script do Bd + modelo de dados

--- db/BancoPostgreSQL.sql
+++ db/BancoPostgreSQL.sql
@@ -2,6 +2,8 @@ CREATE DOMAIN DESC100 VARCHAR(100);
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+-- DDL SPRINT 1
+
 CREATE TABLE Parceiros (
 	parceiro_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
 	parceiro_nome DESC100 NOT NULL,
@@ -30,17 +32,6 @@ CREATE TABLE Administradores(
 	administrador_setor CHAR(1) CHECK (administrador_setor IN ('A', 'B', 'C', 'D')) NOT NULL
 );
 
-CREATE TABLE ConsultorAlianca(
-	consultor_alianca_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
-	consultor_alianca_nome DESC100 NOT NULL,
-	consultor_alianca_cpf DESC100 NOT NULL,
-	consultor_alianca_email DESC100 NOT NULL,
-	consultor_alianca_senha DESC100 NOT NULL,
-	consultor_alianca_status BOOLEAN NOT NULL DEFAULT TRUE
-);
-
--- tabelas sugeridas para sprint 1 (depois sera adicionado mais niveis de qualificao)
-
 CREATE TABLE Trilhas(
 	trilha_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
 	trilha_nome DESC100 NOT NULL
@@ -55,13 +46,40 @@ CREATE TABLE Especializacoes(
 	FOREIGN KEY (id_trilha) REFERENCES Trilhas(trilha_id)
 );
 
-CREATE TABLE ParceiroEspecializacao(
-	par_esp_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
-	par_esp_concluido BOOLEAN DEFAULT FALSE NOT NULL,
-	id_parceiro UUID NOT NULL,
+-- DDL SPRINT 2
+
+CREATE TABLE ConsultorAlianca(
+	consultor_alianca_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+	consultor_alianca_nome DESC100 NOT NULL,
+	consultor_alianca_cpf DESC100 NOT NULL,
+	consultor_alianca_email DESC100 NOT NULL,
+	consultor_alianca_senha DESC100 NOT NULL,
+	consultor_alianca_status BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+CREATE TABLE Qualificadores(
+	qualificador_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+	qualificador_titulo DESC100 NOT NULL,
+	qualificador_descricao DESC100 NOT NULL,
 	id_especializacao UUID NOT NULL,
-	FOREIGN KEY (id_parceiro) REFERENCES Parceiros(parceiro_id),
 	FOREIGN KEY (id_especializacao) REFERENCES Especializacoes(especializacao_id)
+);
+-- obs: em uma próxima sprint podem ser adicionados links dos cursos/certificações (prioridade baixa)
+
+CREATE TABLE ParceiroQualificador(
+	id_parceiro UUID NOT NULL,
+	id_qualificador UUID NOT NULL,
+	FOREIGN KEY (id_parceiro) REFERENCES Parceiros(parceiro_id),
+	FOREIGN KEY (id_qualificador) REFERENCES Qualificadores(qualificador_id),
+	PRIMARY KEY (id_parceiro, id_qualificador)
+);
+
+CREATE TABLE ParceiroTrilha(
+	id_parceiro UUID NOT NULL,
+	id_trilha UUID NOT NULL,
+	FOREIGN KEY (id_parceiro) REFERENCES Parceiros(parceiro_id),
+	FOREIGN KEY (id_trilha) REFERENCES Trilhas(trilha_id),
+	PRIMARY KEY (id_parceiro, id_trilha)
 );
 
 CREATE TABLE AcoesAdministrativas (
@@ -71,13 +89,15 @@ CREATE TABLE AcoesAdministrativas (
 	id_parceiro UUID NULL,
 	id_trilha UUID NULL,
 	id_especializacao UUID NULL,
-	id_par_esp UUID NULL,
+	id_qualificador UUID NULL,
 	FOREIGN KEY (id_administrador) REFERENCES Administradores (administrador_id),
 	FOREIGN KEY (id_parceiro) REFERENCES Parceiros(parceiro_id),
 	FOREIGN KEY (id_trilha) REFERENCES Trilhas(trilha_id),
 	FOREIGN KEY (id_especializacao) REFERENCES Especializacoes(especializacao_id),
-	FOREIGN KEY (id_par_esp) REFERENCES ParceiroEspecializacao(par_esp_id)
+	FOREIGN KEY (id_qualificador) REFERENCES Qualificadores(qualificador_id)
 );
+
+-- DML
 
 --ESPECIALIZACOES DA TRILHA CLOUD BUILD
 INSERT INTO Especializacoes(especializacao_grupo, especializacao_subgrupo, especializacao_nome, id_trilha) 
@@ -360,142 +380,3 @@ VALUES
 ('Serviço', 'Sistemas Projetados', 'Oracle Exadata Database Machine', '95ddcf85-7b47-4811-9c77-46a022243f33'),
 ('Serviço', 'Outros', 'Serviços de Instalação de Hardware da Oracle', '95ddcf85-7b47-4811-9c77-46a022243f33');
 
-/*
-CREATE TABLE Cursos(
-	curso_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
-	curso_nome DESC100 NOT NULL,
-	curso_expertise DESC100 NOT NULL
-);
-*/
-
-/*
-CREATE TABLE CursosConteudo(
-	curso_conteudo_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
-	curso_conteudo_nome DESC100 NOT NULL,
-	curso_conteudo_arquivo DESC100 NULL,
-	curso_conteudo_link DESC100 NULL,
-	curso_conteudo_tipo DESC100 NOT NULL,
-	id_curso UUID NOT NULL,
-	FOREIGN KEY (id_curso) REFERENCES Cursos(curso_id)
-);
-*/
-
-/*
-CREATE TABLE Provas(
-	prova_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
-	prova_nome DESC100 NOT NULL,
-	id_curso UUID NOT NULL,
-	FOREIGN KEY (id_curso) REFERENCES Cursos(curso_id)
-);
-*/
-
-/*
-CREATE TABLE Atividades(
-	atividade_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
-	atividade_nome DESC100 NOT NULL,
-	id_curso UUID NOT NULL,
-	FOREIGN KEY (id_curso) REFERENCES Cursos(curso_id)
-);
-*/
-
-/*
-CREATE TABLE Bonificacoes(
-	bonificacao_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
-	bonificacao_nome DESC100 NOT NULL
-);
-*/
-
-/*
-CREATE TABLE ConteudosProva(
-	conteudo_prova_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
-	conteudo_prova_nome DESC100 NOT NULL,
-	id_prova UUID NOT NULL,
-	FOREIGN KEY (id_prova) REFERENCES Provas(prova_id)
-);
-*/
-
-/*
-CREATE TABLE ConteudosAtividades(
-	conteudo_atividade_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
-	conteudo_atividade_nome DESC100 NOT NULL,
-	id_atividade UUID NOT NULL,
-	FOREIGN KEY (id_atividade) REFERENCES Atividades(atividade_id)
-);
-*/
-
-/*
-CREATE TABLE ParceirosProvas (
-	parceiro_prova_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
-	parceiro_prova_progressao DESC100 NULL,
-	parceiro_prova_nota DESC100 NULL,
-	id_prova UUID NOT NULL,
-	id_parceiro UUID NOT NULL,
-	id_curso UUID NOT NULL,
-	FOREIGN KEY (id_prova) REFERENCES Provas(prova_id),
-	FOREIGN KEY (id_parceiro) REFERENCES Parceiros(parceiro_id),
-	FOREIGN KEY (id_curso) REFERENCES Cursos(curso_id)
-);
-*/
-
-/*
-CREATE TABLE ParceirosAtividades (
-	parceiro_atividade_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
-	parceiro_atividade_progressao DESC100 NULL,
-	parceiro_atividade_nota DESC100 NULL,
-	id_atividade UUID NOT NULL,
-	id_parceiro UUID NOT NULL,
-	id_curso UUID NOT NULL,
-	FOREIGN KEY (id_atividade) REFERENCES Atividades(atividade_id),
-	FOREIGN KEY (id_parceiro) REFERENCES Parceiros(parceiro_id),
-	FOREIGN KEY (id_curso) REFERENCES Cursos(curso_id)
-);
-*/
-
-/*
-CREATE TABLE ParceirosCursos (
-	parceiro_curso_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
-	parceiro_curso_progressao DESC100 NOT NULL,
-	parceiro_curso_nota DESC100 NULL,
-	id_curso UUID NOT NULL,
-	id_parceiro UUID NOT NULL,
-	id_bonificacao UUID NULL,
-	FOREIGN KEY (id_curso) REFERENCES Cursos(curso_id),
-	FOREIGN KEY (id_parceiro) REFERENCES Parceiros(parceiro_id),
-	FOREIGN KEY (id_bonificacao) REFERENCES Bonificacoes(bonificacao_id)
-);
-*/
-
-/*
-CREATE TABLE AcoesAdministrativas (
-	acao_administrativa_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
-	acao_administrativa_tipo DESC100 NOT NULL,
-	id_administrador UUID NOT NULL,
-	id_parceiro UUID NULL,
-	id_prova UUID NULL,
-	id_curso UUID NULL,
-	id_atividade UUID NULL,
-	id_bonificacao UUID NULL,
-	id_curso_conteudo UUID NULL,
-	id_conteudo_prova UUID NULL,
-	id_conteudo_atividade UUID NULL,
-	FOREIGN KEY (id_administrador) REFERENCES Administradores (administrador_id),
-	FOREIGN KEY (id_parceiro) REFERENCES Parceiros(parceiro_id),
-	FOREIGN KEY (id_prova) REFERENCES Provas(prova_id),
-	FOREIGN KEY (id_curso) REFERENCES Cursos(curso_id),
-	FOREIGN KEY (id_atividade) REFERENCES Atividades(atividade_id),
-	FOREIGN KEY (id_bonificacao) REFERENCES Bonificacoes(bonificacao_id),
-	FOREIGN KEY (id_curso_conteudo) REFERENCES CursosConteudo(curso_conteudo_id),
-	FOREIGN KEY (id_conteudo_prova) REFERENCES ConteudosProva(conteudo_prova_id),
-	FOREIGN KEY (id_conteudo_atividade) REFERENCES ConteudosAtividades(conteudo_atividade_id)
-);
-*/
-
-/*
-CREATE TABLE CursosVinculados(
-	curso_vinculados_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
-	id_parceiro UUID NOT NULL,
-	id_curso UUID NOT NULL,
-	FOREIGN KEY (id_parceiro) REFERENCES Parceiros(parceiro_id),
-	FOREIGN KEY (id_curso) REFERENCES Cursos(curso_id)
-);
-*/
