586a7b0e61cb25aca314b2a07089e371c3bdee4c
ThallesTorres <thalles.torres@pm.me>
2024-05-26 23:37:46 -0300

    atualizado dashboard

--- client/src/components/DashboardPartner.tsx
+++ client/src/components/DashboardPartner.tsx
@@ -10,6 +10,28 @@ import { PieChart } from 'react-native-svg-charts';
 
 import { Text as SvgText } from 'react-native-svg';
 
+type TempoMedioConclusaoTrilha = {
+  trilha_id: string;
+  trilha_nome: string;
+  qtotal: number;
+  media: number;
+  total: number;
+  concluiram: number;
+}
+
+type DadosTrilha = {
+  trilha_id: string;
+  trilha_nome: string;
+  qtotal: number;
+  pconcluiram: number;
+}
+
+type ProgressoGeralParceiros = {
+  nao_iniciado: number;
+  em_andamento: number;
+  concluido: number;
+  total: number;
+}
 
 type RootStackParamList = {
   Dashboard: undefined;
@@ -24,7 +46,7 @@ type PartnerCountData = {
 
 type PartnerProgress = {
   parceiro_nome: string;
-  parceiro_id: string 
+  parceiro_id: string
   trilha_nome: string;
   progresso: number;
   count: number;
@@ -37,6 +59,9 @@ const DashboardPartner = () => {
   const chartWidth = screenWidth - 40;
   const navigation = useNavigation<ScreenNavigationProp>();
   const [isSideMenuVisible, setIsSideMenuVisible] = useState(false);
+  const [tempoMedioConclusaoTrilha, setTempoMedioConclusaoTrilha] = useState<TempoMedioConclusaoTrilha[]>([]);
+  const [dadosTrilha, setDadosTrilha] = useState<DadosTrilha[]>([]);
+  const [progressoGeralParceiros, setProgressoGeralParceiros] = useState<ProgressoGeralParceiros | null>(null);
   const [partnerCountData, setPartnerCountData] = useState<PartnerCountData[]>([]);
   const [partnerProgress, setPartnerProgress] = useState<PartnerProgress[]>([]);
   const [parceiroTrilha, setParceiroTrilha] = useState<PartnerProgress[]>([]);
@@ -44,17 +69,50 @@ const DashboardPartner = () => {
   const [selectedTrack2, setSelectedTrack2] = useState<string | null>(null);
   const [selectedTrack3, setSelectedTrack3] = useState<string | null>(null);
   const [partnerDetails, setPartnerDetails] = useState<any>(null);
-  
+
 
   const toggleSideMenu = () => {
     setIsSideMenuVisible(!isSideMenuVisible);
   };
 
   useEffect(() => {
+    fetchTempoMedioConclusaoTrilha();
+    fetchDadosTrilha();
+    fetchProgressoGeralParceiros();
     fetchPartnerProgress();
     fetchPartnerCountByTrack();
     GraficoParceirosPorTrilha()
   }, []);
+
+  const fetchTempoMedioConclusaoTrilha = async () => {
+    try {
+      const response = await fetch(`http://${getIpAddress()}:3001/Dashboard/Grafico7TempoMedioConclusao`);
+      const data = await response.json();
+      setTempoMedioConclusaoTrilha(data);
+    } catch (error) {
+      console.error('Erro ao obter os dados do tempo medio das trilhas (conclusao):', error);
+    }
+  };
+
+  const fetchDadosTrilha = async () => {
+    try {
+      const response = await fetch(`http://${getIpAddress()}:3001/Dashboard/Grafico6TrilhasConcluidas`);
+      const data = await response.json();
+      setDadosTrilha(data);
+    } catch (error) {
+      console.error('Erro ao obter os dados das trilhas:', error);
+    }
+  };
+
+  const fetchProgressoGeralParceiros = async () => {
+    try {
+      const response = await fetch(`http://${getIpAddress()}:3001/Dashboard/Grafico1ParceirosCadastrados`);
+      const data = await response.json();
+      setProgressoGeralParceiros(data);
+    } catch (error) {
+      console.error('Erro ao obter o progresso geral dos parceiros:', error);
+    }
+  };
 
   const fetchPartnerProgress = async () => {
     try {
@@ -119,6 +177,18 @@ const DashboardPartner = () => {
   const colors = ['#33FF57', '#FF5733', '#3366FF', '#FF33CC', '#FFFF33'];
   const colors2 = ['#909090', '#2D572C', '#3366FF', '#CDA434', '#828282'];
 
+  // const temp = [progressoGeralParceiros?.concluido, progressoGeralParceiros?.em_andamento, progressoGeralParceiros?.nao_iniciado]
+  // const PieDataAndamentoParceiros = temp?.map((value: any, index: any) => ({
+  //   value,
+  //   label: ['Concluido', 'Em andamento', 'Não iniciou'],
+  //   key: index, // Usado como identificador único
+  //   svg: {
+  //     fill: colors[index % colors.length],
+  //     onPress: (event: GestureResponderEvent) => handleSectorPress1(event, item.trilha_nome),
+  //     ...(selectedTrack1 === item.trilha_nome && { transform: [{ scale: 1.1 }] })
+  //   },
+  // }));
+
   const TrilhasConcluidas = partnerCountData.map((item: any, index: any) => ({
     value: Number(item.pconcluido),
     label: item.trilha_nome,
@@ -141,7 +211,7 @@ const DashboardPartner = () => {
     },
   }));
   const Labels = ({ slices }) => {
-    return slices.map((slice:any, index:any) => {
+    return slices.map((slice: any, index: any) => {
       const { pieCentroid, data } = slice;
       return (
         <SvgText
@@ -164,56 +234,81 @@ const DashboardPartner = () => {
     DetalhesParceiro(parceiroId); // Chame a função DetalhesParceiro passando o ID do parceiro
     navigation.navigate('DetalhesParceiros', { parceiro_id: parceiroId });
   };
-  
+
   return (
     <>
       <ScrollView style={styles.scrollView}>
-        <ScrollView>
-          <Text style={styles.title}>Progresso de Parceiros</Text>
-          <View style={styles.tableContainer}>
-            <View style={styles.headerRow}>
-              <Text style={styles.header}>Nome</Text>
-              <Text style={styles.header}>Trilha</Text>
-              <Text style={styles.header}>Progresso</Text>
+
+        {/* <Text style={styles.title}>Dados das trilhas</Text>
+          {dadosTrilha.map((data) => (
+            <View>
+              <Text>{data.trilha_nome}: {data.qtotal}</Text>
             </View>
-            {partnerProgress.map((partner, index) => (
-              <View key={index}>
-                <Pressable onPress={() => navigateToPartnerDetails(partner.parceiro_id)}>
-                  <View style={styles.row}>
+          ))} */}
 
-                    <Text style={styles.data}>{partner.parceiro_nome}</Text>
-                    <Text style={styles.data}>{partner.trilha_nome}</Text>
-                    <View style={styles.progressContainer} >
-                      <Progress.Bar
-                        style={styles.progressBar}
-                        progress={partner.progresso}
-                        width={110}
-                        height={15}
+        {/* <Text style={styles.title}>Tempo medio conclusao</Text>
+          {tempoMedioConclusaoTrilha.map((data) => (
+            <View>
+              <Text>{data.trilha_nome}: {data.media ? data.media : 'Dados insuficiente'}</Text>
+            </View>
+          ))} */}
 
-                        borderWidth={1}
-                        color={'#17E753'}
-                      />
-                      <View style={styles.progressBarContent}>
-                        <Text style={styles.bar}>{Math.round(partner.progresso * 100)}%</Text>
-                      </View>
+        <Text style={styles.title}>Progresso de Parceiros</Text>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          <View style={{ flex: 1, alignItems: 'center' }}>
+            <Text style={{ fontSize: 15, color: 'white' }}>Em andamento: {progressoGeralParceiros?.em_andamento}</Text>
+          </View>
+          <View style={{ flex: 1, alignItems: 'center' }}>
+            <Text style={{ fontSize: 15, color: 'white' }}>Concluído: {progressoGeralParceiros?.concluido}</Text>
+          </View>
+          <View style={{ flex: 1, alignItems: 'center' }}>
+            <Text style={{ fontSize: 15, color: 'white' }}>Total: {progressoGeralParceiros?.total}</Text>
+          </View>
+        </View>
+        <View style={styles.tableContainer}>
+          <View style={styles.headerRow}>
+            <Text style={styles.header}>Nome</Text>
+            <Text style={styles.header}>Trilha</Text>
+            <Text style={styles.header}>Progresso</Text>
+          </View>
+
+          {partnerProgress.map((partner, index) => (
+            <View key={index}>
+              <Pressable onPress={() => navigateToPartnerDetails(partner.parceiro_id)}>
+                <View style={styles.row}>
+
+                  <Text style={styles.data}>{partner.parceiro_nome}</Text>
+                  <Text style={styles.data}>{partner.trilha_nome}</Text>
+                  <View style={styles.progressContainer} >
+                    <Progress.Bar
+                      style={styles.progressBar}
+                      progress={partner.progresso}
+                      width={110}
+                      height={15}
+
+                      borderWidth={1}
+                      color={'#17E753'}
+                    />
+                    <View style={styles.progressBarContent}>
+                      <Text style={styles.bar}>{Math.round(partner.progresso * 100)}%</Text>
                     </View>
                   </View>
-                </Pressable>
-              </View>
-            ))}
-          </View>
-        </ScrollView>
+                </View>
+              </Pressable>
+            </View>
+          ))}
+        </View>
         <Text style={styles.title}> Trilhas concluidas pelos parceiros:</Text>
 
-        <View style={{ flex: 1, height: 200,}}>
+        <View style={{ flex: 1, height: 200, }}>
           <PieChart
             style={{ flex: 1 }}
             data={TrilhasConcluidas}
             innerRadius={'50%'} // Define o tamanho do buraco central
             padAngle={0.06} // Define o espaçamento entre os setores
 
-          > 
-          <Labels slices={TrilhasConcluidas} />
+          >
+            <Labels slices={TrilhasConcluidas} />
           </PieChart>
           <View style={styles.legendContainer}>
             {partnerCountData.map((item, index) => (
@@ -244,10 +339,10 @@ const DashboardPartner = () => {
             padAngle={0.06} // Define o espaçamento entre os setores
 
 
-            > 
+          >
             <Labels slices={TrilhasConcluidas} />
-            </PieChart>
-          
+          </PieChart>
+
           <View style={styles.legendContainer}>
             {parceiroTrilha.map((item, index) => (
               <TouchableOpacity
